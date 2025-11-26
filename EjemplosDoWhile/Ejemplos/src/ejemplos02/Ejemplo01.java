a698664fec5ebc833ae6d8db02eeda72eb578a96
poleagui06 <jeanpoleagui06@gmail.com>
2025-11-26 11:36:45 -0500

    Solución problema 1

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
@@ -26,11 +26,15 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         int contador = 1;
         int suma = 0;
+        String cadena = "";
         do {
-        
+            suma = suma + contador;
+            cadena = String.format("%s%d\n", cadena, contador);
             contador = contador + 1;
-        } while (contador <= 10); 
-        System.out.printf("%d\n", suma);
+        } while (contador <= 10);
+        
+        cadena = String.format("%s\nLa suma es %d\n", cadena, suma);
+        System.out.printf("%s", cadena);
     }
 
 }
