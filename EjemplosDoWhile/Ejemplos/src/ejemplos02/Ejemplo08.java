4ec1ec877e5727d866404c5baf0dcb19f7a675a0
poleagui06 <jeanpoleagui06@gmail.com>
2025-11-26 23:09:49 -0500

    Solución problema 8

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo08.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo08.java
@@ -32,9 +32,9 @@ public class Ejemplo08 {
         do {
             // para manejar el signo asociado 
             if (denominador % 2 == 0) {
-                signo = "+";
-            } else {
                 signo = "-";
+            } else {
+                signo = "+";
             }
             
             cadena = String.format("%s%s%d/%d ", 
